d00f73bc1c8001de910519af82467b4e820e9394
Ioannis Kosmadakis <ikosmadakis@protonmail.com>
2025-05-26 10:42:23 +0000

    Rename MainAsset → Asset inside migrations

--- devices/migrations/0001_initial.py
+++ devices/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='MainAsset',
+            name='Asset',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('asset_id', models.CharField(max_length=64)),
